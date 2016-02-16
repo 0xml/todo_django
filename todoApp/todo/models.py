1ed74446bc87c4bfc0042dff63e164885d47937f
Pawel Ropa <pawel.ropa@gmail.com>
2016-02-16 19:52:03 +0100

    Created new migration

--- todoApp/todo/models.py
+++ todoApp/todo/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+	project_name = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+class Task(models.Model):
+	project = models.ForeignKey(Project, on_delete=models.CASCADE)
+	task_name = models.CharField(max_length=200)
+	completed = models.BooleanField(default=False)
