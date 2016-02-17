021f8b5950c830ca45be6f05010b666854e3ff6a
Pawel Ropa <pawel.ropa@gmail.com>
2016-02-17 21:52:36 +0100

    Showing projects

--- todoApp/todo/views.py
+++ todoApp/todo/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
 from .models import Project
 
-def index(response):
+def index(request):
     latest_projects_list = Project.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.project_name for p in latest_projects_list])
-    return HttpResponse(output)
+    template = loader.get_template('todo/index.html')
+    context = {
+        'latest_projects_list': latest_projects_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, project_id):
     return HttpResponse("You're looking at question %s." % project_id)
