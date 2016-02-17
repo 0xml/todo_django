c383f19552a9bc741ea9c143efe1043a52918b9b
Pawel Ropa <pawel.ropa@gmail.com>
2016-02-17 21:33:45 +0100

    Added support for urls

--- todoApp/todo/urls.py
+++ todoApp/todo/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /todo/
     url(r'^$', views.index, name='index'),
+    # ex: /todo/5/
+    url(r'^(?P<project_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /todo/5/tasks/
+    url(r'^(?P<project_id>[0-9]+)/tasks/$', views.results, name='tasks'),
 ]
--- todoApp/todo/views.py
+++ todoApp/todo/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponse
 
 def index(response):
 	return HttpResponse("Todo application")
+
+def detail(request, project_id):
+    return HttpResponse("You're looking at question %s." % project_id)
+
+def tasks(request, project_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % project_id)
