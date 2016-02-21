a6c4753283a69202fe81dbeaa10b9103e2d3bf46
Pawel Ropa <pawel.ropa@gmail.com>
2016-02-21 12:14:46 +0100

    Implemented project details

--- todoApp/todo/views.py
+++ todoApp/todo/views.py
@@ -7,13 +7,15 @@ from .models import Project
 def index(request):
     latest_projects_list = Project.objects.order_by('-pub_date')[:5]
     template = loader.get_template('todo/index.html')
-    context = {
-        'latest_projects_list': latest_projects_list,
-    }
+    context = {'latest_projects_list': latest_projects_list}
     return HttpResponse(template.render(context, request))
 
 def detail(request, project_id):
-    return HttpResponse("You're looking at question %s." % project_id)
+	try:
+		question = Question.objects.get(pk=project_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def tasks(request, project_id):
     response = "You're looking at the results of question %s."
